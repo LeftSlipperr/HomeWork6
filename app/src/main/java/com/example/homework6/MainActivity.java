e27dd67390491ffe47464b976da23f9821104585
First Lesson <coopandquest@gmail.com>
2024-02-29 16:38:34 +0200

    Second

--- app/src/main/java/com/example/homework6/MainActivity.java
+++ app/src/main/java/com/example/homework6/MainActivity.java
@@ -5,18 +5,18 @@ import android.app.Activity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
 
     ListView simpleList;
     String countryList[] = {"India", "China", "Australia", "Portugal", "America", "NewZealand", "Hungary", "France", "Germany", "Japan"};
+    int flags[] = {R.drawable.india, R.drawable.china, R.drawable.australia, R.drawable.portugal, R.drawable.america, R.drawable.new_zealand, R.drawable.hungary, R.drawable.france, R.drawable.germany, R.drawable.japan};
 
-    @Override   protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_main);
-        simpleList = (ListView)findViewById(R.id.simpleListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
-        simpleList.setAdapter(arrayAdapter);
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
+        simpleList.setAdapter(customAdapter);
     }
 }
